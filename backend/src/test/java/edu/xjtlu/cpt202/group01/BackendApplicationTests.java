a597e079163efb0158634b6ab50260b71317ed59
Qiran Xiao <sys2010xiaoqiran@126.com>
2026-03-11 17:29:03 +0800

    init: initialize project structure

---
+++ backend/src/test/java/edu/xjtlu/cpt202/group01/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xjtlu.cpt202.group01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

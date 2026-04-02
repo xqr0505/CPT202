0183ede1d3ba1e2d7c93ce457cdfe27c5e7257c6
Qiran Xiao <sys2010xiaoqiran@126.com>
2026-04-02 23:45:43 +0800

    chore: remove incorrect files

--- backend/src/main/java/edu/xjtlu/cpt202/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/edu/xjtlu/cpt202/backend/common/exception/GlobalExceptionHandler.java
@@ -2,11 +2,7 @@ package edu.xjtlu.cpt202.backend.common.exception;
 
 import edu.xjtlu.cpt202.backend.common.result.Result;
 import edu.xjtlu.cpt202.backend.common.enums.ResultCodeEnum;
-<<<<<<< Updated upstream
 import org.springframework.security.access.AccessDeniedException;
-=======
-import org.springframework.dao.DataIntegrityViolationException;
->>>>>>> Stashed changes
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
 /**
+ * global exception handler
  * @author QiranXiao
  * @date 2026/3/26
  */
@@ -43,22 +40,9 @@ public class GlobalExceptionHandler {
         return Result.fail(ResultCodeEnum.PARAM_ERROR.getCode(), message);
     }
 
-<<<<<<< Updated upstream
-=======
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public Result<Void> handleDataIntegrityViolationException(DataIntegrityViolationException e) {
-        return Result.fail(ResultCodeEnum.BAD_REQUEST.getCode(), "The requested operation violates database constraints");
-    }
-
-    @ExceptionHandler({NoResourceFoundException.class, NoHandlerFoundException.class})
-    public Result<Void> handleNotFoundException(Exception e) {
-        return Result.fail(ResultCodeEnum.NOT_FOUND.getCode(), ResultCodeEnum.NOT_FOUND.getMessage());
-    }
-
->>>>>>> Stashed changes
     @ExceptionHandler(Exception.class)
     public Result<Void> handleException(Exception e) {
-        // Log the actual exception here in a real application
         return Result.fail(ResultCodeEnum.SYSTEM_ERROR.getCode(), ResultCodeEnum.SYSTEM_ERROR.getMessage());
     }
+
 }
